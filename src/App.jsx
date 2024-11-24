7578e4bde0dc75b5ef82858209c5ae37f478a596
Vinay Malyala <srivinay1432@gmail.com>
2024-11-24 14:15:03 +0530

    add about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import Hero from './components/Hero'
+import About from './components/About'
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
       < Hero />
 
-      <section className='z-0 min-h-screen bg-blue-500' />
+      <About />
     </main>
   )
 }
